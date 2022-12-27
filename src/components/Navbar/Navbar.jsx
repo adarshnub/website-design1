21b565bcc933637c73908ff6c74d5502a5a15d76
adarshnub <adarshmanjady@gmail.com>
2022-12-27 12:58:11 +0530

    navbar update

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { GiHamburgerMenu } from 'react-icons/gi';
 import logo from "../../Images/logo.png";
 import logotitle from "../../Images/Archslate.png";
 import icon1 from "../../Images/Icon1.png";
@@ -10,17 +11,26 @@ import icon5 from "../../Images/Icon5.png";
 
 const Navbar = () => {
   return (
-    <div className="bg-white py-2 drop-shadow-md">
-        <nav className=" flex justify-between ">
-        <div className="flex flex-row gap-4 px-8  items-center">
-            <a href="#"><img src={logo} 
+  <div className="bg-white py-2 drop-shadow-md">
+  <div className="flex flex-wrap ">
+  <section className="relative ">
+     {/* navbar */}
+    <nav className="flex justify-between   w-screen">
+      <div className="px-5  py-2 flex w-full  justify-between">
+        <div>
+        <a className="flex px-8 items-center gap-4" href="#">
+        <img src={logo} 
             alt="logo"
-            className="h-12 w-12" /></a>
-            <a href="#"><img src={logotitle}
+            className="h-12 w-12" 
+        />
+        <img src={logotitle}
             alt="logotitle" 
-            className="h-6 w-30" /></a>
-        </div>
-        <div className="flex flex-row space-x-4 gap-4 mr-8 ">
+            className="h-6 w-30" />
+        </a>    
+        </div>    
+          
+        
+        <div className="hidden lg:flex flex-row space-x-4 gap-4 mr-8 ">
             <div className= "flex items-center flex-col">
                 <a href="#"><img src={icon1} alt="icon"  className="w-6 h-6 "/></a>
                 <a href="#"><span className="text-sm font-semibold  ">Candidates</span></a>
@@ -42,7 +52,20 @@ const Navbar = () => {
                 <a href="#"><span className="text-sm font-semibold ">User Name</span></a>
             </div>
         </div>
-        </nav>
+       
+      </div>
+
+      
+     
+      
+      <a className=" self-center mr-12 lg:hidden" href="#">
+      <GiHamburgerMenu />
+      
+      </a>
+    </nav>
+    
+  </section>
+</div> 
 
     </div>
   )
